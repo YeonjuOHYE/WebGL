79803d76c6c68b79b18cefdebfae1873e292dc6c
sgju-placea <sg.ju@placea.co.kr>
2020-12-12 23:21:29 +0900

    [new] three모듈 추가 및 basic 프로젝트 완료

--- WebGL/WebGL/settings.py
+++ WebGL/WebGL/settings.py
@@ -118,7 +118,10 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "main/static"),
+]
 
 MEDIA_ROOT = BASE_DIR / "media"
 
