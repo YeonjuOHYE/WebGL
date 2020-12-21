8ae00b02f142b3dfceb683ed4aa4f839c5595a6d
sgju-placea <sg.ju@placea.co.kr>
2020-12-21 23:57:50 +0900

    globe 2d data parsing and showing

---
+++ WebGL/main/static/js/earch_globe_3d.js
@@ -0,0 +1,81 @@
+async function loadFile(url) {
+    const req = await fetch(url);
+    return req.text()
+}
+function parseData(text) {
+    const data = [];
+    const settings = { data };
+    let max;
+    let min;
+    //split into lines
+    text.split('\n').forEach((line) => {
+        //split the line by whitespace
+        const parts = line.trim().split(/\s+/);
+        if (parts.length === 2) {
+            //only 2 parts, must be a key/value pair
+            settings[parts[0]] = parseFloat(parts[1])
+        }
+        else if (parts.length > 2) {
+            //more than 2 parts, must be data
+            const values = parts.map((v) => {
+                // parts list를 순회하면서 해당하는 파라미터를 넣은 리스트 생성
+                const value = parseFloat(v);
+                // -9999
+                if (value === settings.NODATA_value) {
+                    return undefined;
+                }
+                max = Math.max(max === undefined ? value : max, value);
+                min = Math.min(min === undefined ? value : min, value);
+                return value;
+            });
+            data.push(values)
+        }
+    });
+    // dictionary 재생성. 첫 인자 memory에 추가로 덧붙임
+    return Object.assign(settings, { min, max });
+}
+
+function drawData(file) {
+    const { min, max, ncols, nrows, data } = file;
+    const range = max - min;
+    console.log(min);
+    console.log(max);
+    console.log(ncols);
+    console.log(nrows);
+    console.log(data);
+    var canvas = document.createElement('canvas');
+
+    document.getElementById("threejs_canvas").appendChild(canvas);
+    const ctx = canvas.getContext('2d');
+    // make the canvas the same size as the data
+    ctx.canvas.width = ncols;
+    ctx.canvas.height = nrows;
+    ctx.fillStyle = '#444';
+
+    ctx.canvas.style.width = px(ncols * 2);
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    console.log(file);
+    data.forEach((row, latNdx) => { //145
+        row.forEach((value, lonNdx) => {
+            if (value === undefined) {
+                return;
+            }
+            const amount = (value - min) / range;//0~1 normalize
+            const hue = 0.5;
+            const saturation = 1;
+            const lightness = amount;
+            ctx.fillStyle = hsl(hue, saturation, lightness);
+            // ctx.fillRect(lonNdx * 2, latNdx * 2, 2, 2);
+            ctx.fillRect(lonNdx, latNdx, 1, 1);
+        });
+    });
+}
+function px(v) {
+    return `${v | 0}px`;
+}
+function hsl(h, s, l) {
+    return `hsl(${h * 360 | 0},${s * 100 | 0}%,${l * 100 | 0}%)`;
+}
+loadFile('/media/main/earth_globe/gpw_v4_basic_demographic_characteristics_rev10_a000_14mt_2010_cntm_1_deg.asc')
+    .then(parseData)
+    .then(drawData)
