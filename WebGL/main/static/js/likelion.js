af60cd727915a02f70bdef12f7001142755f0ec1
sugang-alchera <sg.ju@alcherainc.com>
2021-01-13 18:23:52 +0900

    사자 만드는중

--- WebGL/main/static/js/likelion.js
+++ WebGL/main/static/js/likelion.js
@@ -1,7 +1,13 @@
 import { OrbitControls } from '../jsm/controls/OrbitControls.js';
 
 let scene, renderer, camera, controls
-let cube;
+
+let lion
+let body
+let leg_anchor_l;
+let leg_anchor_r;
+let hair_list;
+let beard_list;
 
 start();
 update();
@@ -25,37 +31,108 @@ function start() {
     //set camera for mouse rotation
     controls = new OrbitControls(camera, renderer.domElement)
 
-    const lion = new THREE.Object3D();
+    {
+        //make lion
+        lion = new THREE.Object3D();
+        const body_m = new THREE.MeshPhongMaterial({ color: 0xffd377 });
+        const hair_m = new THREE.MeshPhongMaterial({ color: 0xff553e });
+        const eye_m = new THREE.MeshPhongMaterial({ color: 0xffffff });
+        const nose_m = new THREE.MeshPhongMaterial({ color: 0xb07388 });
+        const mouth_m = new THREE.MeshPhongMaterial({ color: 0x000000 });
 
-    //foot
-    const foot_anchor = new THREE.Object3D();
-    const foot_g = new THREE.BoxGeometry(1, 0.5, 0.5);
-    const body_m = new THREE.MeshPhongMaterial({ color: 0xffd377 });
-    const foot1 = new THREE.Mesh(foot_g, body_m)
-    const foot2 = new THREE.Mesh(foot_g, body_m)
-    const foot3 = new THREE.Mesh(foot_g, body_m)
-    const foot4 = new THREE.Mesh(foot_g, body_m)
-    foot1.position.set(0.55, 0, 0)
-    foot2.position.set(-0.55, 0, 0)
-    foot3.position.set(2, 0, -0.4)
-    foot4.position.set(-2, 0, -0.4)
-    foot_anchor.add(foot1)
-    foot_anchor.add(foot2)
-    foot_anchor.add(foot3)
-    foot_anchor.add(foot4)
-    lion.add(foot_anchor)
+        //foot
+        const foot_anchor = new THREE.Object3D();
+        const foot_g = new THREE.BoxGeometry(1, 0.5, 0.5);
+        const foot1 = new THREE.Mesh(foot_g, body_m)
+        const foot2 = new THREE.Mesh(foot_g, body_m)
+        const foot3 = new THREE.Mesh(foot_g, body_m)
+        const foot4 = new THREE.Mesh(foot_g, body_m)
+        foot1.position.set(0.55, 0, 0)
+        foot2.position.set(-0.55, 0, 0)
+        foot3.position.set(2, 0, -0.4)
+        foot4.position.set(-2, 0, -0.4)
+        foot_anchor.add(foot1)
+        foot_anchor.add(foot2)
+        foot_anchor.add(foot3)
+        foot_anchor.add(foot4)
+        lion.add(foot_anchor)
 
-    //body
-    const body_g = new THREE.ConeGeometry(2.5, 4, 3);
-    const body = new THREE.Mesh(body_g, body_m);
-    body.position.set(0, 2, -1)
-    body.scale.z = 0.3
-    lion.add(body)
+        //body
+        const body_g = new THREE.ConeGeometry(2.5, 4, 3);
+        body = new THREE.Mesh(body_g, body_m);
+        body.position.set(0, 2, -1)
+        body.scale.z = 0.3
+        lion.add(body)
 
-    //leg
-    const leg_anchor_l = new THREE.Object3D();
-    const leg_anchor_r = new THREE.Object3D();
-    const leg_g = new THREE.BoxGeometry(1, 1, 1)
+        //leg
+        leg_anchor_l = new THREE.Object3D();
+        leg_anchor_r = new THREE.Object3D();
+        const leg_g = new THREE.BoxGeometry(0.7, 2, 0.7)
+        const leg_l = new THREE.Mesh(leg_g, body_m)
+        const leg_r = new THREE.Mesh(leg_g, body_m)
+        leg_anchor_l.position.set(1.8, 0, - 1)
+        leg_l.position.set(0, 1, 0)
+        leg_anchor_l.rotation.z = -15 * Math.PI / 180
+        leg_anchor_l.add(leg_l)
+        leg_anchor_r.position.set(-1.8, 0, - 1)
+        leg_r.position.set(0, 1, 0)
+        leg_anchor_r.rotation.z = 15 * Math.PI / 180
+        leg_anchor_r.add(leg_r)
+        lion.add(leg_anchor_l)
+        lion.add(leg_anchor_r)
+
+        const head_anchor = new THREE.Object3D();
+        //hair
+        const hair_g = new THREE.BoxGeometry(1, 1, 0.3)
+        hair_list = []
+        const hair_anchor = new THREE.Object3D();
+        for (let i = 0; i < 16; i++) {
+            const hair = new THREE.Mesh(hair_g, hair_m)
+            const quotient = (i - i % 4) / 4;
+            const remainder = i % 4;
+            hair.position.set(remainder - 1.5, quotient - 1.5, 0)
+            hair_list.push(hair)
+            hair_anchor.add(hair)
+        }
+        head_anchor.position.set(0, 3.5, -0.5)
+        head_anchor.add(hair_anchor)
+        lion.add(head_anchor)
+
+        //face
+        const face_g = new THREE.BoxGeometry(2.1, 2.1, 2.1)
+        const face_anchor = new THREE.Object3D();
+        const face = new THREE.Mesh(face_g, body_m)
+        face.position.set(0, 0, 1.05)
+
+        //nose
+        const nose_g = new THREE.BoxGeometry(1, 1, 0.5)
+        const nose = new THREE.Mesh(nose_g, nose_m)
+        nose.position.set(0, 0.65, 0.9)
+        face.add(nose)
+
+        //chin
+        const chin_g = new THREE.BoxGeometry(1, 0.35, 0.5)
+        const chin = new THREE.Mesh(chin_g, body_m)
+        chin.position.set(0, -1.225, 0.8)
+        face.add(chin)
+
+        //mouth
+        const mouth_g = new THREE.BoxGeometry(0.25, 0.25, 0.1)
+        const mouth = new THREE.Mesh(mouth_g, mouth_m)
+        mouth.position.set(0, -0.8, 1.1)
+        face.add(mouth)
+
+        //beard
+        // const beard_g = new THREE.BoxGeometry(0.25, 0.25, 0.1)
+        // beard_list = []
+        // const mouth = new THREE.Mesh(mouth_g, mouth_m)
+        // mouth.position.set(0, -0.8, 1.1)
+        // face.add(mouth)
+
+        face_anchor.add(face)
+        head_anchor.add(face_anchor)
+    }
+
 
     scene.add(lion)
 
@@ -78,8 +155,10 @@ function start() {
 //update
 function update() {
     requestAnimationFrame(update);
-    const speed = 0.01
+    const speed = 1
 
+    // leg_anchor_l.rotation.z += speed * Math.PI / 180
+    // body.rotation.y += speed * Math.PI / 180
     controls.update();
     renderer.render(scene, camera);
 };
