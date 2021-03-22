e0cc22edfb87d68d6d4f99385fd7cdeac6435295
sgju-placea <sg.ju@placea.co.kr>
2021-03-23 01:34:27 +0900

    localhost

--- WebGL/WebGL/settings.py
+++ WebGL/WebGL/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "s)t^*yhz$xw4d6=)ebo1-!xrk)(ya7p9ktg%#kpdgneh7&*%+k"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["jusugang.pythonanywhere.com"]
+ALLOWED_HOSTS = ["jusugang.pythonanywhere.com", "127.0.0.1"]
 
 
 # Application definition
--- WebGL/main/static/js/circle_wave.js
+++ WebGL/main/static/js/circle_wave.js
@@ -1,6 +1,6 @@
 let renderer, scene, camera;
 let uniforms;
-let mouseX=0, mouseY=0;
+let mouseX = 0, mouseY = 0;
 start();
 update();
 
@@ -11,45 +11,46 @@ function start() {
   renderer = new THREE.WebGLRenderer({ antialias: true, preserveDrawingBuffer: true });
   camera = new THREE.OrthographicCamera(
     -1, // left
-      1, // right
-      1, // top
+    1, // right
+    1, // top
     -1, // bottom
     -1, // near,
-      1, // far
+    1, // far
   );
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.getElementById("threejs_canvas").appendChild(renderer.domElement);
-  
+
   const plane = new THREE.PlaneGeometry(2, 2);
   const fShader = document.getElementById('fragmentShader').innerHTML;
 
   uniforms = {
     iTime: { value: 0 },
-    iResolution:  { value: new THREE.Vector3() },
-    iMouse:  { value: new THREE.Vector4() },
+    iResolution: { value: new THREE.Vector3() },
+    iMouse: { value: new THREE.Vector4() },
   };
-  
+
   const material = new THREE.ShaderMaterial({
-    uniforms:uniforms,
-    fragmentShader:fShader,
+    uniforms: uniforms,
+    fragmentShader: fShader,
   });
 
   scene.add(new THREE.Mesh(plane, material));
-  
+
   //window resize 대응
   window.addEventListener('resize', onWindowResize, false);
   //mouse move 대응
   document.addEventListener("mousemove", function (event) { mouseMove(event); });
 }
 
-function update(){
-  
+function update() {
+
   const time = requestAnimationFrame(update) * 0.01;
-  
-  uniforms.iResolution.value.set(window.innerWidth,  window.innerHeight, 1);
+
+  uniforms.iResolution.value.set(window.innerWidth, window.innerHeight, 1);
+  console.log(uniforms.iResolution.value);
   uniforms.iTime.value = time;
-  uniforms.iMouse.value.x = lerp(uniforms.iMouse.value.x, mouseX, 0.12) ;
+  uniforms.iMouse.value.x = lerp(uniforms.iMouse.value.x, mouseX, 0.12);
   uniforms.iMouse.value.y = lerp(uniforms.iMouse.value.y, mouseY, 0.12);
 
   renderer.render(scene, camera);
@@ -63,7 +64,7 @@ function onWindowResize() {
 
 function mouseMove(event) {
   mouseX = event.clientX;
-  mouseY = window.innerHeight-event.clientY;
+  mouseY = window.innerHeight - event.clientY;
 }
 
 function lerp(start, end, amt) {
