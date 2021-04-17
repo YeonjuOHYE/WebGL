8fda09fd843834439018872765b2c43c9ac9942f
sgju-placea <sg.ju@placea.co.kr>
2021-04-17 19:17:17 +0900

    add fbx

--- WebGL/main/static/js/colorpicker.js
+++ WebGL/main/static/js/colorpicker.js
@@ -1,11 +1,16 @@
+import { FBXLoader } from '../jsm/loaders/FBXLoader.js';
+
 var colorWell;
-var defaultColor = "#0000ff";
+var defaultColor = "#1d1d1d";
 let renderer, scene, camera;
 let material;
 let mouseX=0, mouseY=0;
 
+let dirLight1;
 let pet;
-let pointer;
+
+let clock = new THREE.Clock();
+let mixer;
 
 function THREE_start() {
   console.log("JustColor !")
@@ -17,24 +22,45 @@ function THREE_start() {
   scene.background = new THREE.Color(defaultColor);
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMap.enabled = true;
   document.getElementById("threejs_canvas").appendChild(renderer.domElement);
-  
-  const pointer_g = new THREE.BoxGeometry(1,1)
-  const pointer_m = new THREE.MeshPhongMaterial({
-    color:0xebe533
-  });
-  pointer = new THREE.Mesh(pointer_g, pointer_m);
-  scene.add(pointer);
 
+  camera.position.set(0, 0, 3);
+  camera.lookAt(0, 0, 0);
+
+  //add pet
   pet = new THREE.Object3D();
-  const face = new THREE.BoxGeometry(1, 1);
-  material = new THREE.MeshPhongMaterial({});
-  scene.add(new THREE.Mesh(face, material));
-  
-  // lights
-  const dirLight1 = new THREE.DirectionalLight(0x602733);
-  dirLight1.position.set(0, 1, 0);
+  // model
+  const loader = new FBXLoader();
+  loader.load( '/media/main/colorpicker/windmill.fbx', function ( object ) {
+
+    object.scale.set(0.01,0.01,0.01)
+    object.position.set(0,-0.3,0)
+    object.rotation.set(15 * Math.PI/180,80 * Math.PI/180,0)
+    pet.add( object );
+
+  } );
+
+  scene.add(pet);
+  pet.position.y = 0.4
+
+  // add lights
+  dirLight1 = new THREE.DirectionalLight(defaultColor);
+  dirLight1.position.set(0,0, 1);
+  dirLight1.intensity = 0.5;
   scene.add(dirLight1);
+  const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
+  hemiLight.position.set( 0, 200, 0 );
+  scene.add( hemiLight );
+
+  // const dirLight = new THREE.DirectionalLight( 0xffffff );
+	// 			dirLight.position.set( 0, 200, 100 );
+	// 			dirLight.castShadow = true;
+	// 			dirLight.shadow.camera.top = 180;
+	// 			dirLight.shadow.camera.bottom = - 100;
+	// 			dirLight.shadow.camera.left = - 120;
+	// 			dirLight.shadow.camera.right = 120;
+	// 			scene.add( dirLight );
 
   const ambientLight = new THREE.AmbientLight(0x8c8c8c);
   scene.add(ambientLight);
@@ -46,18 +72,21 @@ function THREE_start() {
 }
 
 function THREE_update(){
-  const time = requestAnimationFrame(THREE_update) * 0.01;
+  requestAnimationFrame(THREE_update) * 0.01;
 
-  //update pointer 3d position
-  pointer.position.set(mouseX,mouseY,0); 
-  
+  const delta = clock.getDelta();
+  if ( mixer ) mixer.update( delta );
 
-  // head_anchor.lookAt(propeller)
+  pet.rotation.y = lerp(pet.rotation.y, mouseX, 0.2)
+  pet.rotation.x = lerp(pet.rotation.x, mouseY, 0.2)
+
   colorWell = document.querySelector("#colorWell");
   const col = colorWell.value;
   const b = new THREE.Color(col)
-  // console.log(col.replace("#",''));
-  material.color = b;
+  
+  // material.color = b;\
+  dirLight1.color = b;
+  scene.background = b;
   renderer.render(scene, camera);
 }
 
@@ -68,8 +97,8 @@ function onWindowResize() {
 }
 
 function mouseMove(event) {
-  mouseX = event.clientX;
-  mouseY = window.innerHeight-event.clientY;
+  mouseX = event.clientX * 2 / window.innerWidth - 1; //-1 ~1
+  mouseY = event.clientY * 2 / window.innerHeight - 1; //-1 ~1
 }
 
 function lerp(start, end, amt) {
@@ -92,9 +121,9 @@ function startup() {
       document.querySelectorAll("h3").forEach(function(p) {
           p.style.color = event.target.value;
       });
+      
     }, false);
-    colorWell.select();
-
+    
     THREE_start();
     THREE_update();
 }
